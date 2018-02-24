903a1bec7bff7c742b89372d09bb22c9b34c111c
Dmitry Mamonov <dmitry.mamonov@dataart.com>
2018-02-24 19:19:06 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,36 @@
 'use strict';
 
-var toggler = document.getElementById('toggler');
-var mainNav = document.querySelector('.main-nav');
-var headerTitle = document.querySelector('.page-header__title');
+document.addEventListener("DOMContentLoaded", function() {
+  var toggler = document.getElementById('toggler');
+  var mainNav = document.querySelector('.main-nav');
+  var headerTitle = document.querySelector('.page-header__title');
+  var buttonToShowHiddenTextAboutAuthor = document.querySelector('.about__description-button');
+  var hiddenTextAboutAuthor = document.querySelector('.about__description--hidden');
 
-function mainNavVisibleToggle(event) {
-  event.preventDefault();
-  toggler.classList.toggle('toggler--close');
-  mainNav.classList.toggle('main-nav--active');
-  headerTitle.classList.toggle('page-header__title--transform');
-}
+  function mainNavVisibleToggle(event) {
+    event.preventDefault();
+    toggler.classList.toggle('toggler--close');
+    mainNav.classList.toggle('main-nav--active');
+    headerTitle.classList.toggle('page-header__title--transform');
 
-if (toggler) {
-  toggler.addEventListener('click', mainNavVisibleToggle);
-}
+    if (toggler.classList.contains('toggler--close')) {
+      toggler.setAttribute('aria-pressed', 'true');
+    } else {
+      toggler.setAttribute('aria-pressed', 'false');
+    }
+  }
+
+  if (toggler) {
+    toggler.addEventListener('click', mainNavVisibleToggle);
+  }
+
+  function showMoreHiddenTextAboutAuthor(event) {
+    event.preventDefault();
+    hiddenTextAboutAuthor.classList.toggle('about__description--hidden');
+    buttonToShowHiddenTextAboutAuthor.classList.toggle('about__description-button--transformed');
+  }
+
+  if (buttonToShowHiddenTextAboutAuthor) {
+    buttonToShowHiddenTextAboutAuthor.addEventListener('click', showMoreHiddenTextAboutAuthor);
+  }
+});
